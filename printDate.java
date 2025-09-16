b79473bf176c0bc8cbaed3c81289a507a7a115d1
LemonPrism <charliechenvan@gmail.com>
2025-09-16 14:59:49 -0700

    Add files via upload

---
+++ printDate.java
@@ -0,0 +1,28 @@
+
+
+public class printDate
+{
+    
+    public static void printAmerican(String day, int date, String month, int year){
+        
+        System.out.printf("%s,%s, %d,%d%n" , day, month ,date ,year);
+        
+    }
+    
+    public static void printEuropean(String day, int date, String month, int year){
+        
+
+        System.out.printf("%s,%d,%s,%d%n" , day, date ,month ,year);
+        
+        
+    }
+    
+    public static void main (){
+        
+        printAmerican("Monday", 22, "July", 2019); 
+        
+        printEuropean("Monday", 22, "July", 2019); 
+        
+        
+    }
+}
